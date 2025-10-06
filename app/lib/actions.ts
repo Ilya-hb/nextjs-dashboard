5cee65a0e92a3783ef16fee36ba19489eeea3712
ilyholu <ilyholu@gmail.com>
2025-10-06 12:49:43 +0300

    Working with forms & mutating data via server actions, handling errors via error.tsx

---
+++ app/lib/actions.ts
@@ -0,0 +1,89 @@
+"use server";
+// By adding the 'use server', you mark all the exported functions within the file as Server Actions.
+//  These server functions can then be imported and used in Client and Server components.
+//  Any functions included in this file that are not used will be automatically removed from the final application bundle.
+import { z } from "zod";
+import postgres from "postgres";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(["pending", "paid"]),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split("T")[0];
+  console.log(customerId, amount, status, date, amountInCents);
+  try {
+    await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId},${amountInCents},${status},${date})
+  `;
+  } catch (error) {
+    if (error instanceof Error)
+      console.log("Error while creating invoice: ", error.message);
+    else console.log(error);
+    return {
+      message: "DB error: Failed to create invoice",
+    };
+  }
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+  const amountInCents = amount * 100;
+  try {
+    await sql`
+  UPDATE invoices
+  SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+  `;
+  } catch (error) {
+    if (error instanceof Error)
+      console.log("Error while updating invoice: ", error.message);
+    else console.log(error);
+    return {
+      message: "DB error: Failed to update invoice",
+    };
+  }
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
+
+export async function deleteInvoice(id: string) {
+  try {
+    await sql`
+  DELETE FROM invoices WHERE id = ${id}`;
+  } catch (error) {
+    if (error instanceof Error)
+      console.log("Error while deleting invoice: ", error.message);
+    else console.log(error);
+    return {
+      message: "DB error: Failed to delete invoice",
+    };
+  }
+
+  revalidatePath("/dashboard/invoices");
+}
